1999fbb21ac547342fa91f775ec6f3b4d247a61f
shouzia <saopipizhu@163.com>
2022-07-23 10:17:09 +0800

    🐛 完善获取关键字

--- __init__.py
+++ __init__.py
@@ -21,21 +21,21 @@ __plugin_settings__ = {
     "cmd": ["下载", "cdlinux", "破解", "kali"],
 }
 
-download = on_regex("下载", block=True, aliases={"下载", "官网"}, priority=5)
+download = on_regex("^(下载)$", block=True, aliases={"下载", "官网"}, priority=5)
 
 
 @download.handle()
 async def download_escape():
     await download.send("通道h5ai:https://h5ai.108878.xyz/\n""通道官网:https://hypin.cn/download")
 
-cdlinux = on_regex("cdlinux", block=True, aliases={"cdlinux"}, priority=5)
+cdlinux = on_regex("^(cdlinux)$", block=True, aliases={"cdlinux"}, priority=5)
 
 
 @cdlinux.handle()
 async def cdlinux_escape():
     await cdlinux.send("CDlinux破解教程:\nhttps://blog.shouzi.xyz/cdlinux-wifi/")
 
-kali = on_regex("kali", block=True, aliases={"kali"}, priority=5)
+kali = on_regex("^(kali)$", block=True, aliases={"kali"}, priority=5)
 
 
 @kali.handle()
@@ -43,7 +43,8 @@ async def kali_escape():
     await kali.send("Kali破解教程:\nhttps://blog.shouzi.xyz/kali/")
 
 
-pojie = on_regex("破解", block=True, aliases={"电脑破解", "破解", "水滴"}, priority=5)
+pojie = on_regex("^(破解)$", block=True, aliases={
+                 "电脑破解", "破解", "水滴"}, priority=5)
 
 
 @pojie.handle()
